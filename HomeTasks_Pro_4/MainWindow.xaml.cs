2ddd628419c0bc3cb6ac28091d9941bc34fdef65
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-21 18:08:28 +0300

    Complete Task2

--- HomeTasks_Pro_4/MainWindow.xaml.cs
+++ HomeTasks_Pro_4/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using Microsoft.VisualBasic;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -17,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace HomeTasks_Pro_4
 {
@@ -25,6 +27,7 @@ namespace HomeTasks_Pro_4
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		const string FILE = "test.txt";
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -36,34 +39,54 @@ namespace HomeTasks_Pro_4
 			WebClient client = new WebClient();
 			string html = client.DownloadString(url);
 			string linkPattern = @"<a\s+[^>]*href\s*=\s*['""]([^'""]*)['""][^>]*>";
-			MatchCollection linkMatches = Regex.Matches(html, linkPattern);
-			StringBuilder stringBuilder = new StringBuilder();
-			if (linkMatches.Count > 0)
-			{
-				foreach (Match match in linkMatches)
-				{
-					stringBuilder.Append(match.Groups[1].Value);
-				}
-			}
+			string phonePattern = @"\+\d{12}";
+			string emailPattern = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b";
 
-			using (StreamWriter writer = new StreamWriter("test.txt"))
-			{
-				writer.WriteLine(stringBuilder.ToString());
-			}
+			MatchCollection linkMatches = Regex.Matches(html, linkPattern);
+			MatchCollection phoneMatches = Regex.Matches(html, phonePattern);
+			MatchCollection emailMatches = Regex.Matches(html, emailPattern);
+
+			List<string> strings = new List<string>();
+			strings.Add("Ланки на веб странці:");
+			strings.AddRange(WriteMatches(linkMatches));
+			strings.Add("Телефони на веб странці:");
+			strings.AddRange(WriteMatches(phoneMatches));
+			strings.Add("Емейли на веб странці:");
+			strings.AddRange(WriteMatches(emailMatches));
+
+			WriteToFile(FILE, strings);
 		}
 
+		private List<string> WriteMatches(MatchCollection matches)
+		{
+			List<string> strings = new List<string>();
+			if (matches.Count > 0)
+			{
+				foreach (Match match in matches)
+				{
+					strings.Add(match.Value);
+				}
+			}
+			return strings;
+		}
+
+		private void WriteToFile(string fileName, List<string> strings)
+		{
+			using (StreamWriter writer = new StreamWriter(fileName))
+			{
+				foreach (string match in strings)
+				{
+					writer.WriteLine(match);
+				}
+			}
+		}
 		private void OpenTxtButton_Click(object sender, RoutedEventArgs e)
 		{
-			string filePath = "test.txt"; 
+			string filePath = FILE;
 
-			try
-			{
-				Process.Start(filePath);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine("Произошла ошибка при открытии файла: " + ex.Message);
-			}
+			string fileContent = File.ReadAllText(filePath);
+
+			ShowTextBox.Text = fileContent;
 		}
 	}
 }
