0c58af923a2e501fe614fe5eef64d8560d78c019
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-21 18:53:36 +0300

    Complete Task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Task6
+﻿using System.Text.RegularExpressions;
+
+namespace Task6
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			string loginPattern = @"^[A-Za-z0-9]{4,20}$";
+            Console.WriteLine("Введить ваш логін");
+            string login = Console.ReadLine();
+
+			if (Regex.IsMatch(login, loginPattern))
+			{
+                Console.WriteLine("Реєстрація пройшла успішно");
+            }
+			else
+			{
+				Console.WriteLine("Реєстрація не пройшла");
+			}
 		}
 	}
 }
